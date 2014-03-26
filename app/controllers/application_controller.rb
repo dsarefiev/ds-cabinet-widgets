fefe37ad9da02f26741884ae8776ddc880edbb34
Andrey Arefiev <aaarefiev@dasreda.ru>
2014-03-26 17:46:11 +0400

    авторизация сос тороны виджета

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,41 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from Uas::Error, with: :user_error
+  rescue_from Uas::NotAuthorized, with: :user_not_authorized
+
+  protected
+
+  def user_error
+    render 'errors/user_error'
+    allow_iframe
+  end
+
+  def user_not_authorized
+    render 'errors/not_auth'
+    allow_iframe
+  end
+
+  private
+
+  def allow_iframe
+    response.headers.delete 'X-Frame-Options'
+  end
+
+  # @return [Uas::User] currently logged in user
+  def current_user
+    token = request.cookies['auth_token']
+    @current_user ||= Uas::User.find_by_token(token) if token
+    rescue Uas::Error
+      nil
+  end
+
+  helper_method :current_user
+
+  # Add this method to before_action to authorize access.
+  # @example before_filter :authorize
+  def authorize
+    raise Uas::NotAuthorized if current_user.nil?
+  end
 end
--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -1,24 +1,16 @@
 class PurchaseController < ApplicationController
-
+  before_action :authorize
   after_action :allow_iframe
 
   def index
     token = request.cookies['auth_token']
     @message = "виджет клиента: token=" + token
     render :widget
-
   end
 
   def concierge
     @message = "виджет консьержа"
     render :widget
-  end
-
-  private
-
-  def allow_iframe
-    # response.headers['X-Frame-Options'] = 'ALLOW-FROM https://sredda.ru'
-    response.headers.delete 'X-Frame-Options'
   end
 
 end
