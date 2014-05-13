e52dc398e15290bc5425d5944303b900ce7799f8
Andrey Arefiev <aaarefiev@dasreda.ru>
2014-05-13 21:19:17 +0400

    загрузка содержимого корзины в виджете

--- app/models/widgets.rb
+++ app/models/widgets.rb
@@ -1,2 +1,10 @@
 class Widgets < ActiveRecord::Base
+
+  def cart
+    @cart ||= {
+      summary: Ds::Cart::Api.get_cart_summary(client_siebel_id),
+      items: Ds::Cart::Api.get_cart_items(client_siebel_id)
+    }
+  end
+
 end
