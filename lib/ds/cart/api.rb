ab2b592cd14316c973977acb33eb026ba1e69504
Andrey Arefiev <aaarefiev@dasreda.ru>
2014-05-13 20:53:57 +0400

    доделки и обработка ошибок API корзины

--- lib/ds/cart/api.rb
+++ lib/ds/cart/api.rb
@@ -1,14 +1,11 @@
-require 'singleton'
-
-#TODO
+require 'ds/cart/error'
 
 module Ds
   module Cart
 
   class Api < Ds::Cart::Base
-    include Singleton
 
-    def method_missing(method_name, *args)
+    def self.method_missing(method_name, *args)
       url = 'api/' + method_name.to_s.gsub('_', '/')
       request = {}
       args[0].each do |param, value|
@@ -17,30 +14,43 @@ module Ds
       response = Ds::Cart::Query.execute(url, request: request, method: :get)
       case response[:code]
         when 200 then JSON.parse(response[:body])
-        when 400 then raise InvalidCredentials
         else raise InternalError
       end
     end
 
-    def get_cart_summary(user_id, client_session_key)
+    def self.get_cart_summary(user_id)
       url = 'api/items/summary'
-      request = { userId: user_id, clientSessionKey: client_session_key}
+      request = { userId: user_id}
       response = Ds::Cart::Query.execute(url, request: request, method: :get)
       case response[:code]
         when 200 then JSON.parse(response[:body])
-        when 400 then raise InvalidCredentials
         else raise InternalError
       end
     end
 
 
-    def get_cart_items(user_id)
+    def self.get_cart_items(user_id)
       url = 'api/items'
       request = { userId: user_id }
       response = Ds::Cart::Query.execute(url, request: request, method: :get)
       case response[:code]
         when 200 then JSON.parse(response[:body])
-        when 400 then raise InvalidCredentials
+        else raise InternalError
+      end
+    end
+
+    def self.add_to_cart(offering, article_url)
+      url = 'api/products'
+      request = {
+          Offering: nil,
+          SerializedOffering: offering.to_json,
+          ArticleUrl: article_url,
+          ArticleId: nil
+        }
+      response = Ds::Cart::Query.execute(url, request: request, method: :post)
+      case response[:code]
+        when 201 then JSON.parse(response[:body])
+        when 500 then raise InternalError, JSON.parse(response[:body])["ErrorMessage"]
         else raise InternalError
       end
     end
